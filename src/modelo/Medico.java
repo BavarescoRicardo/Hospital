5708a6681737dccdaa5c9789cf876873809f34f7
ricardo <ricardo.bav17@gmail.com>
2019-06-13 21:51:55 -0300

    Auditoria para medico

--- src/modelo/Medico.java
+++ src/modelo/Medico.java
@@ -9,12 +9,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
+import org.hibernate.envers.Audited;
 /**
  *
  * @author Ricardo
  */
+
 @Entity
+@Audited(modifiedColumnName = "verificaMed")
+@Table
 public class Medico {
     @Id 
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -24,7 +28,7 @@ public class Medico {
     private String CPF;
     private String ESPECIALIDADE;
     private String EMAIL;
-    //private java.sql.Date dataNascimento;
+    
 
     public int getIdMedico() {
         return idMedico;
